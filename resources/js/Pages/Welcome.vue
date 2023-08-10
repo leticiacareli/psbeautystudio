146375e1aa5c19ec3d2ae88ad3fdf57832c17050
leticiacareli <leticia_careli@hotmail.com>
2023-08-10 11:06:41 -0300

    First Commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+    import { Head, Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+
+    <Head title="Welcome" />
+
+    <h1 class="text-3xl font-bold underline text-red-600">
+        Hello world!
+    </h1>
+</template>
